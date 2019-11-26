34b04555a4511940f47061a0bf9c1e4be5a2e3ce
Xhjay <xhj97@icloud.com>
2019-11-26 15:17:14 +0800

    完成用户列表功能开发

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import store from './store'
 import './plugins/element.js'
 import router from './router'
-
 // 导入全局样式
 import './assets/css/global.css'
 
@@ -14,9 +13,16 @@ import axios from 'axios'
 
 Vue.config.productionTip = false
 
+// 挂载confirm
+
 Vue.prototype.$http = axios
 // 配置请求的根路径
 axios.defaults.baseURL = `http://127.0.0.1:8888/api/private/v1/`
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  console.log(config)
+  return config
+})
 
 new Vue({
   store,
